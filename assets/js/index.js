435ac0c742e85d2e9c19390218dd074a825f58d0
Navya Malhotra <navya0807@gmail.com>
2023-11-02 19:34:09 +0530

    culinary page commits

---
+++ assets/js/index.js
@@ -0,0 +1,23 @@
+// const faders = document.querySelectorAll('.fadein');
+
+// const appearOnScroll = new IntersectionObserver
+
+// const appearOptions ={
+//     threshold: 1,
+//     rootMargin: "0px 0px -100px 0px"
+// }
+// (function(enteries, appearOnScroll) {
+//     enteries.forEach(entry => {
+//         if(!entry.isIntersecting){
+//             return;
+//         }
+//         else {
+//             entry.target.classList.add('appear');
+//             appearOnScroll.unobserve(entry.target);
+//         }
+//     })
+// },appearOptions)
+
+// faders.forEach(fader => {
+//     appearOnScroll.observe(fader);
+// });
