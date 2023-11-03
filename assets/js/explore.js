9b90c511cb03b51287a313f877a62f00030b5041
BSRanjitha <bs.ranjuprasad@gmail.com>
2023-11-03 06:58:06 +0530

    explore front end

--- assets/js/explore.js
+++ assets/js/explore.js
@@ -86,128 +86,128 @@ if(state == "Karnataka"){
 };
 }
 
-else if (state == "Punjab")
+else if (state == "Jammu and Kashmir")
 {
      restaurant = {
         data:[
        {
-            rname: "Punjabi Grand",
-            location: "Kumarswamy Layout",
-            price: "Rs.200 Per Person",
-            dish: "Dosa",
-            image: "../assets/img/img1.1.jpeg",
+            rname: "Kundan Da Dhaba",
+            location: "Address : Shalamar, Prem Nagar, Old Heritage City, Jammu and Kashmir 180001",
+            price: "Approx Cost : Rs.200",
+            dish: "Speciality : Phulka and Curry",
+            image: "../assets/img/exp2.1.jpeg",
        },
        {
-        rname: "Punjabi Swaad",
-        location: "Kumarswamy Layout",
-        price: "Rs.200 Per Person",
-        dish: "Dosa",
-        image: "../assets/img/img1.2.jpeg",
+        rname: "Vaishno Dhaba",
+        location: "Address : City Centre Road, Near Sangam Cinema, Jammu and Kashmir 143001",
+        price: "Approx Cost : Rs.200",
+        dish: "Speciality : Lachedar Paratha and Dal Makhani",
+        image: "../assets/img/exp2.2.jpeg",
        },
        {
-        rname: "Udupi Swaad",
-        location: "Kumarswamy Layout",
-        price: "Rs.200 Per Person",
-        dish: "Dosa",
-        image: "../assets/img/img1.3.jpeg",
+        rname: "Pahalwan’s",
+        location: "Address : Gandhi Nagar, Jammu, Jammu and Kashmir 180004",
+        price: "Approx Cost : Rs.200",
+        dish: "Speciality : Khaladi Kulcha, Chole Bature",
+        image: "../assets/img/exp2.3.jpeg",
        },
        {
-        rname: "Udupi Swaad",
-        location: "Kumarswamy Layout",
-        price: "Rs.200 Per Person",
-        dish: "Dosa",
-        image: "../assets/img/img1.4.jpeg",
+        rname: "ZAMINDARA DHABA",
+        location: "Address : Raya Morh, Jakh, Bari Brahmana, Samba, Jammu and Kashmir 181133",
+        price: "Approx Cost : Rs.200",
+        dish: "Speciality : Butter Naan and Paneer Makhani",
+        image: "../assets/img/exp2.4.jpeg",
        },
        {
-        rname: "Udupi Swaad",
-        location: "Kumarswamy Layout",
-        price: "Rs.200 Per Person",
-        dish: "Dosa",
-        image: "../assets/img/img1.5.jpeg",
-       },
-    ],
-    };
-}
-
-else if (state == "Delhi"){
-     restaurant = {
-        data:[
-       {
-            rname: "Udupi Grand",
-            location: "Kumarswamy Layout",
-            price: "Rs.200 Per Person",
-            dish: "Dosa",
-            image: "../assets/img/img1.1.jpeg",
-       },
-       {
-        rname: "Udupi Swaad",
-        location: "Kumarswamy Layout",
-        price: "Rs.200 Per Person",
-        dish: "Dosa",
-        image: "../assets/img/img1.2.jpeg",
-       },
-       {
-        rname: "Udupi Swaad",
-        location: "Kumarswamy Layout",
-        price: "Rs.200 Per Person",
-        dish: "Dosa",
-        image: "../assets/img/img1.3.jpeg",
-       },
-       {
-        rname: "Udupi Swaad",
-        location: "Kumarswamy Layout",
-        price: "Rs.200 Per Person",
-        dish: "Dosa",
-        image: "../assets/img/img1.4.jpeg",
-       },
-       {
-        rname: "Udupi Swaad",
-        location: "Kumarswamy Layout",
-        price: "Rs.200 Per Person",
-        dish: "Dosa",
-        image: "../assets/img/img1.5.jpeg",
+        rname: "Jammu Darbar",
+        location: "Address : Stadium Morh, Channi Himmat, Bye Pass, Jammu, Jammu and Kashmir 180015",
+        price: "Approx Cost : Rs.200",
+        dish: "Speciality : Kashmiri Pulav",
+        image: "../assets/img/exp2.5.jpeg",
        },
     ],
     };
 }
 
-else if (state == "Kerala") {
+else if (state == "Telangana"){
      restaurant = {
         data:[
        {
-            rname: "Udupi Grand",
-            location: "Kumarswamy Layout",
-            price: "Rs.200 Per Person",
-            dish: "Dosa",
-            image: "../assets/img/img1.1.jpeg",
+            rname: "Hotel Shadab",
+            location: "Address : High Court Road, Charminar, Ghansi Bazaar, Hyderabad",
+            price: "Approx Cost : Rs.200",
+            dish: "Speciality : Nihari, Payas, Biryani",
+            image: "../assets/img/exp4.1.jpeg",
        },
        {
-        rname: "Udupi Swaad",
-        location: "Kumarswamy Layout",
-        price: "Rs.200 Per Person",
-        dish: "Dosa",
-        image: "../assets/img/img1.2.jpeg",
+        rname: "Hotel Nayab",
+        location: "Address : Nayapul Road, Nassir Complex, Chatta Bazar, Darulshifa, Hyderabad",
+        price: "Approx Cost : Rs.200",
+        dish: "Speciality : Paya, Khichdi, Keema",
+        image: "../assets/img/exp4.2.jpeg",
        },
        {
-        rname: "Udupi Swaad",
-        location: "Kumarswamy Layout",
-        price: "Rs.200 Per Person",
-        dish: "Dosa",
-        image: "../assets/img/img1.3.jpeg",
+        rname: "Cafe Niloufer",
+        location: "Address : Redhills, Lakdikapul, Hyderabad",
+        price: "Approx Cost : Rs.200",
+        dish: "Speciality : Irani chai, Hyderabadi Biryani,Mutton Haleem ",
+        image: "../assets/img/exp4.3.webp",
        },
        {
-        rname: "Udupi Swaad",
-        location: "Kumarswamy Layout",
-        price: "Rs.200 Per Person",
-        dish: "Dosa",
-        image: "../assets/img/img1.4.jpeg",
+        rname: "ZAMINDARA DHABA",
+        location: "Address : Raya Morh, Jakh, Bari Brahmana, Samba, Hyderabad",
+        price: "Approx Cost : Rs.200",
+        dish: "Speciality : Butter Naan and Paneer Makhani",
+        image: "../assets/img/exp2.4.jpeg",
        },
        {
-        rname: "Udupi Swaad",
-        location: "Kumarswamy Layout",
-        price: "Rs.200 Per Person",
-        dish: "Dosa",
-        image: "../assets/img/img1.5.jpeg",
+        rname: "Moti Mahal",
+        location: "Address : Kalupur Rd, near Railway Station, Kapasia Bazar, Sakar Bazzar, Kalupur, Hyderabad",
+        price: "Approx Cost : Rs.200",
+        dish: "Speciality : Chicken Tangri and Biriyani",
+        image: "../assets/img/exp3.2.jpeg",
+       },
+    ],
+    };
+}
+
+else if (state == "Gujarat") {
+     restaurant = {
+        data:[
+       {
+            rname: "Chandravilas Restaurant",
+            location: "Address : Gandhi Rd, near Ratan pol, Old City, Danapidth, Khadia, Ahmedabad",
+            price: "Approx Cost : Rs.200",
+            dish: "JSpeciality : alebi, Rasmalai",
+            image: "../assets/img/exp3.1.jpeg",
+       },
+       {
+        rname: "Moti Mahal",
+        location: "Address : Kalupur Rd, near Railway Station, Kapasia Bazar, Sakar Bazzar, Kalupur, Ahmedabad",
+        price: "Approx Cost : Rs.200",
+        dish: "Speciality : Chicken Tangri and Biriyani",
+        image: "../assets/img/exp3.2.jpeg",
+       },
+       {
+        rname: "Raipur Bhajiya House",
+        location: "Address : Opposite Radhe Mall, Khokhra Circle, Ahmedabad",
+        price: "Approx Cost : Rs.200",
+        dish: "Speciality : Bhajiyas and Jalebi",
+        image: "../assets/img/exp3.3.jpeg",
+       },
+       {
+        rname: "The Jungle Bhookh",
+        location: "Address : Wall Street-1, 103/B,, Shahid Veer Kinariwala Marg, opp. Orient Club, Ellis Bridge, Ahmedabad",
+        price: "Approx Cost : Rs.200",
+        dish: "Speciality : Malai Kofta, Paneer Butter Masala",
+        image: "../assets/img/exp3.4.jpeg",
+       },
+       {
+        rname: "Agashiye",
+        location: "Address : The House of Mangaldas Girdhardas, Opp. Sidi Saiyad Jali, Lal Darwaja,, Ahmedabad",
+        price: "Approx Cost : Rs.200",
+        dish: "Speciality : Farsan, Kadhi, Sweets",
+        image: "../assets/img/exp3.5.jpeg",
        },
     ],
     };
@@ -217,39 +217,39 @@ else{
      restaurant = {
         data:[
        {
-            rname: "Vidyarthi Bhawan",
-            location: "Kumarswamy Layout",
-            price: "Rs.200 Per Person",
-            dish: "Dosa",
-            image: "../assets/img/exp1.jpeg",
+            rname: "Mahamaya",
+            location: "Address : Matilal Nehru Road and, Ananda Ram Baruah Rd, Pan Bazaar, Guwahati, Assam 781001",
+            price: "Approx Cost : Rs.200",
+            dish: "Speciality : Paratha Sabji, Fish Thali",
+            image: "../assets/img/exp5.1.jpeg",
        },
        {
-        rname: "Udupi Swaad",
-        location: "Kumarswamy Layout",
-        price: "Rs.200 Per Person",
-        dish: "Dosa",
-        image: "../assets/img/img1.2.jpeg",
+        rname: "Reboti Chat House",
+        location: "Address : Gopinath Bordoloi Rd, Pan Bazaar, Guwahati, Assam 781001",
+        price: "Approx Cost : Rs.200",
+        dish: "Speciality : Reboti Special Chowmein",
+        image: "../assets/img/exp5.2.jpeg",
        },
        {
-        rname: "Udupi Swaad",
-        location: "Kumarswamy Layout",
-        price: "Rs.200 Per Person",
-        dish: "Dosa",
-        image: "../assets/img/img1.3.jpeg",
+        rname: "Ashoka Restaurant",
+        location: "Address : H B Road, Pan Bazaar, Guwahati - 781001 ",
+        price: "Approx Cost : Rs.200",
+        dish: "Speciality : Kalakand, Kheer Pitha, and Kata Nimkee",
+        image: "../assets/img/exp5.4.jpeg",
        },
        {
-        rname: "Udupi Swaad",
-        location: "Kumarswamy Layout",
-        price: "Rs.200 Per Person",
-        dish: "Dosa",
-        image: "../assets/img/img1.4.jpeg",
+        rname: "Lakhi Cabin",
+        location: "Address : Hem Baruah Rd, Kamarpatty, Fancy Bazaar, Guwahati, Assam 781001",
+        price: "Approx Cost : Rs.200",
+        dish: "Speciality : Aloo-Matar Samosa, Masala Kachori",
+        image: "../assets/img/exp5.3.jpeg",
        },
        {
-        rname: "Udupi Swaad",
-        location: "Kumarswamy Layout",
-        price: "Rs.200 Per Person",
-        dish: "Dosa",
-        image: "../assets/img/img1.5.jpeg",
+        rname: "Hotel Nayab",
+        location: "Address : Nayapul Road, Nassir Complex, Chatta Bazar, Darulshifa, Hyderabad",
+        price: "Approx Cost : Rs.200",
+        dish: "Speciality : Paya, Khichdi, Keema",
+        image: "../assets/img/exp4.2.jpeg",
        },
     ],
     };
