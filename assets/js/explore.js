c344f8acaf2adfb52f14d5412598950a815dbb5d
BSRanjitha <bs.ranjuprasad@gmail.com>
2023-11-05 14:48:39 +0530

    spell

--- assets/js/explore.js
+++ assets/js/explore.js
@@ -17,7 +17,7 @@ if(state == "Karnataka"){
         location: "DESCRIPTION : Fermented crepe made from rice batter and black lentils and you stuff it with a lightly cooked filling of potatoes, fried onions and spices.",
         origin: "ORIGIN : Udupi",
         price: "APPROX COST : Rs.100",
-        dish: "RESRTAURANT : Vidyarthi Bhawan(Bengaluru)",
+        dish: "RESTAURANT : Vidyarthi Bhawan(Bengaluru)",
         image: "../assets/img/exp1.jpeg",
    },
    {
@@ -25,7 +25,7 @@ if(state == "Karnataka"){
     location: "DESCRIPTION : Consists of sardines semi-stewed in a curry with assorted vegetables. Usually served with rice, naan, or bread",
     origin: "ORIGIN : Mangaluru",
     price: "APPROX COST : Rs.150",
-    dish: "RESRTAURANT : Machali(Mangaluru)",
+    dish: "RESTAURANT : Machali(Mangaluru)",
     image: "../assets/img/exp2.jpeg",
    },
    {
@@ -33,7 +33,7 @@ if(state == "Karnataka"){
     location: "DESCRIPTION : Similar to a buttery and dense cookie, it is made of generous amounts of ghee, sugar, gram flour, and often cardamom",
     origin: "ORIGIN : Mysore",
     price: "APPROX COST : Rs.150/kg",
-    dish: "RESRTAURANT : Mansukhlal Mittaiwala(Mysore)",
+    dish: "RESTAURANT : Mansukhlal Mittaiwala(Mysore)",
     image: "../assets/img/exp4.jpeg",
    },
    {
@@ -41,7 +41,7 @@ if(state == "Karnataka"){
     location: "DESCRIPTION : Savoury fritter-type snack made from rice flour, cream of wheat or rava",
     origin: "ORIGIN : Maddur",
     price: "APPROX COST : Rs.50",
-    dish: "RESRTAURANT : Mavalli Tiffin Room(Bengaluru)",
+    dish: "RESTAURANT : Mavalli Tiffin Room(Bengaluru)",
     image: "../assets/img/exp3.jpeg",
    },
    {
@@ -49,7 +49,7 @@ if(state == "Karnataka"){
     location: "DESCRIPTION : Indian bread made of sorghum bicolor consumed along with spicy curry or chutney",
     origin: "ORIGIN : Bijapur",
     price: "APPROX COST : Rs.50",
-    dish: "RESRTAURANT : Rotti Mattu(Bengaluru)",
+    dish: "RESTAURANT : Rotti Mattu(Bengaluru)",
     image: "../assets/img/exp5.jpeg",
    },
 ],
@@ -67,7 +67,7 @@ else if(state == "Jammu and Kashmir"){
            location: "DESCRIPTION :A must try dish for all the Lamb or meat lovers, Rogan Josh, an aromatic lamb dish is one of the signature recipes of Kashmiri cuisine. ",
            origin: "ORIGIN : srinagar",
            price: "APPROX COST : Rs.400",
-           dish: "RESRTAURANT : Shamyana restaurant(Srinagar)",
+           dish: "RESTAURANT : Shamyana restaurant(Srinagar)",
            image: "../assets/img/Rogan-Josh.jpg",
       },
       {
@@ -75,7 +75,7 @@ else if(state == "Jammu and Kashmir"){
        location: "DESCRIPTION : This dish is sweet, flavoured and healthy with saffron as the main spice which gives it beautiful colour and taste.",
        origin: "ORIGIN : yusmarg",
        price: "APPROX COST : Rs.150",
-       dish: "RESRTAURANT : Kashmiri Wazwan(yusmarg)",
+       dish: "RESTAURANT : Kashmiri Wazwan(yusmarg)",
        image: "../assets/img/exp2.2.jpeg",
       },
       {
@@ -83,7 +83,7 @@ else if(state == "Jammu and Kashmir"){
        location: "DESCRIPTION : Served on festivals and occasions, Kashmir gaad is a dish made up of fish prepared generally with radish or nadur. ",
        origin: "ORIGIN : Sonamerg",
        price: "APPROX COST : Rs.350/kg",
-       dish: "RESRTAURANT : Ahdoos Restaurant(Sonamerg)",
+       dish: "RESTAURANT : Ahdoos Restaurant(Sonamerg)",
        image: "../assets/img/exp2.3.jpeg",
       },
       {
@@ -91,7 +91,7 @@ else if(state == "Jammu and Kashmir"){
        location: "DESCRIPTION :  Lyodur Tschaman is cottage cheese cooked in creamy turmeric based gravy. ",
        origin: "ORIGIN : Dal Lake",
        price: "APPROX COST : Rs.110",
-       dish: "RESRTAURANT : Vishal Dhaba(Dal Lake)",
+       dish: "RESTAURANT : Vishal Dhaba(Dal Lake)",
        image: "../assets/img/exp2.4.jpeg",
       },
       {
@@ -99,7 +99,7 @@ else if(state == "Jammu and Kashmir"){
        location: "DESCRIPTION : Wazwan, the traditional cuisine of Kashmir, is a gastronomic delight that showcases the rich flavors and aromatic spices of the region.",
        origin: "ORIGIN :yusmarg",
        price: "APPROX COST : Rs.50",
-       dish: "RESRTAURANT : Kashmiri Wazwan(yusmarg)",
+       dish: "RESTAURANT : Kashmiri Wazwan(yusmarg)",
        image: "../assets/img/exp2.5.jpeg",
       },
    ],
@@ -115,7 +115,7 @@ else if(state == "Jammu and Kashmir"){
            location: "DESCRIPTION :It is a savoury pancake made of rice flour, chana dal, peanuts and chilli. It is cooked in a deep, round shaped pan. ",
            origin: "ORIGIN : Hyderabad",
            price: "APPROX COST : Rs.100",
-           dish: "RESRTAURANT : Deccan Pavilion(Hyderabad)",
+           dish: "RESTAURANT : Deccan Pavilion(Hyderabad)",
            image: "../assets/img/exp3.1.jpeg",
       },
       {
@@ -123,7 +123,7 @@ else if(state == "Jammu and Kashmir"){
        location: "DESCRIPTION :As Telangana cuisine is well-known for its spices, this dish incorporates succulent pieces of mutton into a thick gravy that is loaded with locally grown spices and cooked over low flame for hours ",
        origin: "ORIGIN :  Warangal",
        price: "APPROX COST : Rs.250",
-       dish: "RESRTAURANT : Paradise Restaurant(Warangal)",
+       dish: "RESTAURANT : Paradise Restaurant(Warangal)",
        image: "../assets/img/exp3.2.jpeg",
       },
       {
@@ -131,7 +131,7 @@ else if(state == "Jammu and Kashmir"){
        location: "DESCRIPTION :Ultra-thin flattened Indian bread with a stuffing made of jaggery, channa dal, cardamom powder and ghee, Polelu is a go to recipe for special festivals like Ganesh Chathurthi.",
        origin: "ORIGIN :  Nizamabad",
        price: "APPROX COST : Rs.50/piece",
-       dish: "RESRTAURANT : Ahdoos Restaurant( Nizamabad)",
+       dish: "RESTAURANT : Ahdoos Restaurant( Nizamabad)",
        image: "../assets/img/exp3.3.jpeg",
       },
       {
@@ -139,7 +139,7 @@ else if(state == "Jammu and Kashmir"){
        location: "DESCRIPTION :  Thanks to the major influence of Nizams in the state, Hyderabadi Biryani has come to be known worldwide as one of India’s jeweled food items. ",
        origin: "ORIGIN : Hyderabad",
        price: "APPROX COST : Rs.110",
-       dish: "RESRTAURANT : biryani Dhaba(Hyderabad)",
+       dish: "RESTAURANT : biryani Dhaba(Hyderabad)",
        image: "../assets/img/exp3.4.jpeg",
       },
       {
@@ -147,7 +147,7 @@ else if(state == "Jammu and Kashmir"){
        location: "DESCRIPTION : A tea-time accompaniment, Chegodilu is a crunchy snack item that is almost impossible to stop munching on after the first bite. ",
        origin: "ORIGIN : Begumpet",
        price: "APPROX COST : Rs.50",
-       dish: "RESRTAURANT : Saffron Soul(Begumpet)",
+       dish: "RESTAURANT : Saffron Soul(Begumpet)",
        image: "../assets/img/exp3.5.jpeg",
       },
    ],
@@ -167,7 +167,7 @@ else if(state == "Jammu and Kashmir"){
            location: "DESCRIPTION :Gujarat's most frequently cooked and savored cuisines are the soft, spongy, and square-shaped pieces served with chutney.",
            origin: "ORIGIN : Gujarat.",
            price: "APPROX COST : Rs.100",
-           dish: "RESRTAURANT : Deccan Pavilion(Gujarat)",
+           dish: "RESTAURANT : Deccan Pavilion(Gujarat)",
            image: "../assets/img/img1.5.jpeg",
       },
       {
@@ -175,7 +175,7 @@ else if(state == "Jammu and Kashmir"){
        location: "DESCRIPTION :Like thin Parathas served for breakfast, Theplas are flatbreads made of fenugreek leaves, gram flour, and many spices. ",
        origin: "ORIGIN :  Kutch",
        price: "APPROX COST : Rs.250",
-       dish: "RESRTAURANT : Anand Restaurants(Kutch)",
+       dish: "RESTAURANT : Anand Restaurants(Kutch)",
        image: "../assets/img/exp4.3.jpeg",
       },
       {
@@ -183,7 +183,7 @@ else if(state == "Jammu and Kashmir"){
        location: "DESCRIPTION :Unlike normal Khichdi, there’s something about Gujarati Khichdi that makes it incredibly delicious. There’s a reason why it’s called the national food of India.",
        origin: "ORIGIN :  Ahmedabad",
        price: "APPROX COST : Rs.50/piece",
-       dish: "RESRTAURANT : Ahdoos Restaurant( Ahmedabad)",
+       dish: "RESTAURANT : Ahdoos Restaurant( Ahmedabad)",
        image: "../assets/img/exp4.4.jpeg",
       },
       {
@@ -191,7 +191,7 @@ else if(state == "Jammu and Kashmir"){
        location: "DESCRIPTION :  Texture-wise, both are the same, but Dabeli tastes slightly different.  ",
        origin: "ORIGIN :Kutch",
        price: "APPROX COST : Rs.110",
-       dish: "RESRTAURANT : dabeli Dhaba(Kutch)",
+       dish: "RESTAURANT : dabeli Dhaba(Kutch)",
        image: "../assets/img/exp4.5.jpeg",
       },
       {
@@ -199,7 +199,7 @@ else if(state == "Jammu and Kashmir"){
        location: "DESCRIPTION : Traditionally, the jalebi recipe is made with a batter of all purpose flour, gram flour (besan), baking soda and water that is fermented for several hours. ",
        origin: "ORIGIN : Ahmedabad",
        price: "APPROX COST : Rs.20",
-       dish: "RESRTAURANT : Saffron Soul(Ahmedabad)",
+       dish: "RESTAURANT : Saffron Soul(Ahmedabad)",
        image: "../assets/img/exp4.1.jpeg",
       },
    ],
@@ -223,7 +223,7 @@ else if(state == "Jammu and Kashmir"){
            location: "DESCRIPTION :Momos, a popular Asian snack, are dumplings filled with savory ingredients, often served with a delicious dipping sauce..",
            origin: "ORIGIN : Guwahati",
            price: "APPROX COST : Rs.100",
-           dish: "RESRTAURANT : Deccan Pavilion(Guwahati)",
+           dish: "RESTAURANT : Deccan Pavilion(Guwahati)",
            image: "../assets/img/exp5.1.jpeg",
       },
       {
@@ -231,7 +231,7 @@ else if(state == "Jammu and Kashmir"){
        location: "DESCRIPTION :A household favourite, this graceful dish of Assamese cuisine is extremely refreshing on the palette. The fish is slow cooked in a rich,tangy broth made with tomato, outenga(elephant ear), and lemon. ",
        origin: "ORIGIN :  Kaziranga",
        price: "APPROX COST : Rs.250",
-       dish: "RESRTAURANT : Anand Restaurants(kaziranga)",
+       dish: "RESTAURANT : Anand Restaurants(kaziranga)",
        image: "../assets/img/exp5.2.jpeg",
       },
       {
@@ -239,7 +239,7 @@ else if(state == "Jammu and Kashmir"){
        location: "DESCRIPTION :When it comes to main food of Assam, Khaar is a non vegetarian dish that comes on top. This meat delicacy is made from the main ingredient khaar on which it is named and also has raw papaya, pulses, and taro.",
        origin: "ORIGIN :  kaziranga",
        price: "APPROX COST : Rs.50/piece",
-       dish: "RESRTAURANT : Ahdoos Restaurant( Kaziranga)",
+       dish: "RESTAURANT : Ahdoos Restaurant( Kaziranga)",
        image: "../assets/img/exp5.3.jpeg",
       },
       {
@@ -247,7 +247,7 @@ else if(state == "Jammu and Kashmir"){
        location: "DESCRIPTION :  Another one of Assam’s special food items, duck meat is a dish generally reserved for special occasions. If cooked properly, the dish can blow you away thanks to the use of whole spices.",
        origin: "ORIGIN :guwahati",
        price: "APPROX COST : Rs.210",
-       dish: "RESRTAURANT : dabeli Dhaba(guwahati)",
+       dish: "RESTAURANT : dabeli Dhaba(guwahati)",
        image: "../assets/img/exp5.4.jpeg",
       },
       {
@@ -255,7 +255,7 @@ else if(state == "Jammu and Kashmir"){
        location: "DESCRIPTION : This is the Assamese version of mashed potatoes and is an excellent side dish for lunch and dinner.  ",
        origin: "ORIGIN : Kaziranga",
        price: "APPROX COST : Rs.20",
-       dish: "RESRTAURANT : Saffron Soul(Kaziranga)",
+       dish: "RESTAURANT : Saffron Soul(Kaziranga)",
        image: "../assets/img/exp5.5.jpeg",
       },
    ],
