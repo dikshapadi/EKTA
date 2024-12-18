6169061d632120d4c4f1527750a5a50b3b11ecb8
dikshapadi <dikshapadi@gmail.com>
2024-12-18 14:39:45 +0530

    cSMN

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const ecommRoutes = require("./routes/ecommerce.js")
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'assets')));
-
+app.set('views', path.join(__dirname, 'views'));
 require("dotenv").config();
 require("./config/dbConnection.js");
 require("./config/passport.js")(passport);
